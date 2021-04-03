2872b700bd1f965adaf6c9b8ff9935fe79fdc116
FlippedK <72911923+FlippedK@users.noreply.github.com>
2021-04-03 09:54:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,47 +3,64 @@ document.onkeydown = checkButton;
 let cube = {
     x: 0,
     y: 0,
-    id: "main"
+    id: "cube",
+    width: 56,
+    height: 56,
+    trn: .25,
+    step: 50,
+    color: 'black'
 }
 
-function checkButton(e) {
+let map = {
+    width: 900,
+    height: 900
+}
+
+function checkButton(event) {
     
-   let k = e ||  window.event;
+    if (event.keyCode === 38) {
+        // key: "ArrowUp" 
 
-   console.log(k)
-
-   if (k.keyCode === 38) {
-     // key: "ArrowUp" 
-    cube.y -= 50;
-    render(cube.id, cube.x, cube.y);
-    console.log("ArrowUp");
+        if (cube.y  > 0) {
+        cube.y -= cube.step;
+        }
+        console.log("ArrowUp");
     }
-
-   else if (k.keyCode === 40) {
+    
+    if (event.keyCode === 40) {
        // key: "ArrowDown" 
-       cube.y += 50;
-       render(cube.id, cube.x, cube.y);
-       console.log("ArrowDown");
+        if (cube.y < map.height) {
+        cube.y += cube.step;
+        }
+        console.log("ArrowDown");
     }
 
-   else if (k.keyCode === 37) {
+    if (event.keyCode === 37) {
        // key: "ArrowLeft"
-       cube.x -= 50;
-       render(cube.id, cube.x, cube.y);
+       if (cube.x  > 0) {
+            cube.x -= cube.step;
+       }
        console.log("ArrowLeft");
     }
 
-   else if (k.keyCode === 39) {
+    if (event.keyCode === 39) {
         // key: "ArrowRight"
-        cube.x += 50;
-        render(cube.id, cube.x, cube.y);
+        if (cube.x < map.width) {
+        cube.x += cube.step;
+        }
         console.log("ArrowRight");
     }
+
+    render(cube);
 }
 
-function render(id, x, y) {
-    document.getElementById("main").style.top = y + 'px';
-    document.getElementById("main").style.left = x + 'px';
+function render(cube) {
+    document.getElementById(cube.id).style.top = cube.y + 'px';
+    document.getElementById(cube.id).style.left = cube.x + 'px';
+    document.getElementById(cube.id).style.transition = 'ease' + cube.trn + 's';
+    document.getElementById(cube.id).style.width = cube.width + 'px';
+    document.getElementById(cube.id).style.height = cube.height + 'px';
+    document.getElementById(cube.id).style.backgroundColor = cube.color;
 }
 
-render(cube.id, cube.x, cube.y);
+render(cube);
