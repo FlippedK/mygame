2d871e6c6d871961923757a1310a68ea04b291b3
FlippedK <72911923+FlippedK@users.noreply.github.com>
2021-04-17 10:02:21 +0500

    botttt

--- lib.js
+++ lib.js
@@ -4,12 +4,13 @@ function checkButton(event) {
 
     let cubeRight = cube.x + cube.width;
     let cubeBottom = cube.y + cube.height;
-    
+
     let mapRight = map.x + map.width;
     let mapBottom = map.y + map.height;
-    
+
+
     if (event.keyCode === 38) {
-        // key: "ArrowUp" 
+        // key: "ArrowUp"
         if (map.y < cube.y) {
             if ((cube.y - map.y) < cube.step) {
                 cube.y -= cube.y - map.y;
@@ -20,24 +21,25 @@ function checkButton(event) {
             }
         }
     }
-    
-    if (event.keyCode === 40) {
-       // key: "ArrowDown" 
-       if (cubeBottom < mapBottom) {
-            if((mapBottom - cubeBottom) < cube.step) {
+
+    else if (event.keyCode === 40) {
+        // key: "ArrowDown"
+        if (cubeBottom < mapBottom) {
+            if ((mapBottom - cubeBottom) < cube.step) {
                 cube.y += mapBottom - cubeBottom;
             }
 
             else {
                 cube.y += cube.step;
             }
-        }   
+        }
     }
 
-    if (event.keyCode === 37) {
-       // key: "ArrowLeft"
-       if (map.x < cube.x) {
-            if ((cube.x - map.x ) < cube.step) {
+    else if (event.keyCode === 37) {
+        // key: "ArrowLeft"
+        
+        if (map.x < cube.x) {
+            if ((cube.x - map.x) < cube.step) {
                 cube.x -= cube.x - map.x;
             }
 
@@ -47,10 +49,10 @@ function checkButton(event) {
         }
     }
 
-    if (event.keyCode === 39) {
+    else if (event.keyCode === 39) {
         // key: "ArrowRight"
-        if ((cube.x + cube.width) < mapRight) {
-            if((mapRight - cubeRight) < cube.step) {
+        if (cubeRight < mapRight) {
+            if ((mapRight - cubeRight) < cube.step) {
                 cube.x += mapRight - cubeRight;
             }
 
@@ -66,7 +68,7 @@ function checkButton(event) {
 function renderCube(cube) {
     document.getElementById(cube.id).style.top = cube.y + 'px';
     document.getElementById(cube.id).style.left = cube.x + 'px';
-    document.getElementById(cube.id).style.transition = 'ease' + cube.trn + 's';
+    document.getElementById(cube.id).style.transition = 'ease ' + cube.trn + 's';
     document.getElementById(cube.id).style.width = cube.width + 'px';
     document.getElementById(cube.id).style.height = cube.height + 'px';
     document.getElementById(cube.id).style.backgroundColor = cube.color;
@@ -78,4 +80,38 @@ function renderMap(map) {
     document.getElementById('map').style.width = map.width + 'px';
     document.getElementById('map').style.height = map.height + 'px';
     document.getElementById('map').style.backgroundColor = map.color;
+}
+
+function spawnCube(map, cube) {
+
+    let mapMinX = map.x;
+    let mapMaxX = map.x + map.width - cube.width;
+
+    let mapMinY = map.y;
+    let mapMaxY = map.y + map.height - cube.height;
+
+    cube.x = Math.round(mapMinX + Math.random() * (mapMaxX - mapMinX));
+    cube.y = Math.round(mapMinY + Math.random() * (mapMaxY - mapMinY));
+
+
+    return cube;
+}
+
+function renderBots(bots) {
+    bots.map(bot => {
+        let div = document.createElement('div');
+        div.id = bot.id;
+        div.className = 'bot';
+        document.body.append(div);
+        renderBot(bot);
+    });
+}
+
+function renderBot(bot) {
+    document.getElementById(bot.id).style.top = bot.y + 'px';
+    document.getElementById(bot.id).style.left = bot.x + 'px';
+    document.getElementById(bot.id).style.transition = 'ease ' + bot.trn + 's';
+    document.getElementById(bot.id).style.width = bot.width + 'px';
+    document.getElementById(bot.id).style.height = bot.height + 'px';
+    document.getElementById(bot.id).style.backgroundColor = bot.color;
 }
